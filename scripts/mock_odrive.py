41eabfa7a3abb10b1b04d5051077ce03948c072f
JesseDarr <darr.jesse@gmamil.com>
2025-06-29 15:17:43 -0500

    cleaned up mock odrive file

--- scripts/mock_odrive.py
+++ scripts/mock_odrive.py
@@ -19,16 +19,12 @@ class MockOdrive(Node):
 
         self.bus      = can.interface.Bus(channel="can0", bustype="socketcan")
         self.notifier = can.Notifier(self.bus, [self._on_can_frame])
-
-        # per-axis position store
         self.position = {nid: 0.0 for nid in NODE_IDS}
-
-        # timer for periodic TX (heartbeat + encoder estimates)
         self.create_timer(DT, self._publish)
 
     def _on_can_frame(self, msg: can.Message):
         node_id = (msg.arbitration_id >> 5) & 0x3F
-        cmd     =  msg.arbitration_id        & 0x1F
+        cmd     =  msg.arbitration_id       & 0x1F
 
         if cmd == CMD_SET_INPUT and node_id in NODE_IDS:
             self.position[node_id] = struct.unpack_from("<f", msg.data)[0]             # payload layout: <f H h B>  (pos, vel_ff, current_ff, ctrl_mode)
@@ -36,27 +32,15 @@ class MockOdrive(Node):
     def _publish(self):
         for nid in NODE_IDS:
             pos = self.position[nid]
-            vel = 0.0                        # simple: velocity unknown/zero
+            vel = 0.0
 
             # 1) Heartbeat (axis_state = CLOSED_LOOP_CONTROL)
             hb = struct.pack("<IBBBx", 0, 8, 0, 1)
-            self.bus.send(
-                can.Message(
-                    arbitration_id=arb_id(nid, CMD_HEARTBEAT),
-                    is_extended_id=False,
-                    data=hb,
-                )
-            )
+            self.bus.send(can.Message(arbitration_id=arb_id(nid, CMD_HEARTBEAT), is_extended_id=False, data=hb))
 
             # 2) Encoder estimates
             enc = struct.pack("<ff", pos, vel)
-            self.bus.send(
-                can.Message(
-                    arbitration_id=arb_id(nid, CMD_ENCODER),
-                    is_extended_id=False,
-                    data=enc,
-                )
-            )
+            self.bus.send(can.Message(arbitration_id=arb_id(nid, CMD_ENCODER), is_extended_id=False, data=enc))
 
 def main():
     try:
