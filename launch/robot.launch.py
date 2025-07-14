491c6d4e022c1100fdadaca2c195073890efe378
JesseDarr <darr.jesse@gmamil.com>
2025-07-13 21:34:59 -0500

    updated robot.launch.py, needs testing

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -5,32 +5,62 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-PKG         = 'darm_ros2'
-URDF_XACRO  = os.path.join(get_package_share_directory(PKG), 'description', 'darm.urdf.xacro')
-RVIZ_CONFIG = os.path.join(get_package_share_directory(PKG), 'rviz', 'darm.rviz')
-
 def generate_launch_description():
-    robot_description = xacro.process_file(URDF_XACRO).toxml()
 
-    rsp_node = Node(
+    # Convience variables
+    pkg_name   = 'darm_ros2'
+    xacro_file = os.path.join(get_package_share_directory(pkg_name), 'description', 'darm.urdf.xacro')
+    robot_desc = xacro.process_file(xacro_file).toxml()
+
+    # Robot State Publisher
+    robot_state_publisher = Node(
         package    = 'robot_state_publisher',
         executable = 'robot_state_publisher',
         output     = 'screen',
-        parameters = [{'robot_description': robot_description}]
+        parameters = [{'robot_description': robot_desc}]
     )
 
-    jsp_node = Node(
-        package    = 'joint_state_publisher',
-        executable = 'joint_state_publisher',
-        output     = 'screen'
-    )
+    # Ros2_Control - Controllers
+    controller_names = ['joint_state_broadcaster', 'arm_controller', ]
+    spawner_nodes    = [
+        Node(
+            package    = 'controller_manager',
+            executable = 'spawner',
+            arguments  = [name],
+            output     = 'screen'
+        ) for name in controller_names
+    ]
 
-    rviz_node = Node(
-        package    = 'rviz2',
-        executable = 'rviz2',
-        arguments  = ['-d', RVIZ_CONFIG],
+    # Joy Stick
+    joy = Node(
+        package    = 'joy',
+        executable = 'joy_node',
+        name       = 'joy_node',
         output     = 'screen',
-        emulate_tty = True
+        parameters = [{
+            'device': '/dev/input/event0',
+            'deadzone': 0.05,
+            'autorepeat_rate': 30.0
+        }]
     )
 
-    return LaunchDescription([rsp_node, jsp_node, rviz_node])
+    # Teleop
+    teleop = Node(
+        package     = 'darm_ros2',
+        executable  = 'teleop_ps5.py',
+        name        = 'teleop_ps5',
+        output      = 'screen',
+        emulate_tty = True,
+        arguments   = [
+            '--ros-args',                
+            '--log-level', 'teleop_ps5:=debug'  
+        ],
+    )
+
+    # Run the nodes
+    return LaunchDescription([
+        robot_state_publisher, 
+        joy,
+        teleop,
+        spawner_nodes
+    ])
